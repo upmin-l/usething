9de119507c9fe92af6ac316ab72179373c9e5662
upmin-l <lanshikeng@uinnova.com>
2022-07-01 17:50:27 +0800

    first commit

---
+++ packages/.vitepress/config.ts
@@ -0,0 +1,28 @@
+export default {
+    title: 'VueUse',
+    description: 'Collection of essential Vue Composition Utilities',
+    ignoreDeadLinks: true,
+    themeConfig:{
+        docsDir: 'packages',
+        editLinks: true,
+        nav:[
+            {
+                text: 'Add-ons',
+                link: '/add-ons',
+              },
+              {
+                text: 'Playground',
+                link: 'https://play.vueuse.org',
+              },
+        ],
+        sidebar: [
+            {
+                text: 'Guide',
+                items: [
+                    { text: 'Introduction', link: '/introduction' },
+                    { text: 'Getting Started', link: '/getting-started' },
+                ]
+            }
+        ]
+    }
+}
---
+++ packages/index.md
@@ -0,0 +1,20 @@
+layout: home
+heroImage: /logo.svg
+actionText: 开始
+
+altActionText: 了解更多
+
+features:
+- title: 💡 Vue3.0 & Vite2
+details: xxv-cli 是一个基于 Vue3.0 生态进行快速开发的完整系统
+- title: ⚡️ 快速搭建
+details: 通过 create 命令可创建零配置的vue3+ thingJS 原型开发.
+- title: 🛠️ 自定义预选搭配
+details: 提供uearth多套主题选搭，快速构建vue3+uearth 3D 地图项目
+- title: 📦 自适应大屏
+details: 可自适应大屏(支持提供spray)、组件自动注册、指定目录路由自动生成等.
+- title: 🔩 真实数据生成
+details: 提供全国真实地铁、高铁站点等geojson的数据生成
+- title: 🔑 自定义预选搭配
+details: 可根据业务需求自定义预选搭配，提供router4、spray、vuex、typescript等依赖
+footer: MIT Licensed | Copyright © 2022-present Melan 
