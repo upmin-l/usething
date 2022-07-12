b3ea80b131aa496a2fa10fcc0b88fcc42ca15672
upmin-l <lanshikeng@uinnova.com>
2022-07-12 16:21:37 +0800

    fix:useSubway create

---
+++ packages/utils/useSubway/index.ts
@@ -0,0 +1,16 @@
+
+const srhData = new Map<string,[string]>([
+    ['上海',['3100_drw_shanghai.json']],
+    ['北京',['1100_drw_beijing.json']],
+    ['广州',['4401_drw_guangzhou.json']],
+    ['深圳',['4403_drw_shenzhen.json']],
+    ['武汉',['4201_drw_wuhan.json']],
+    ['天津',['1200_drw_tianjin.json']],
+    ['南京',['3201_drw_nanjing.json']],
+    ['香港',['8100_drw_xianggang.json']],
+])
+const url = 'http://map.amap.com/service/subway'
+export const useSubway = () => {
+
+}
+
